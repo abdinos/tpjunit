5829c54644dde2472081faebc801bd0aa04f2ea5
o18025131 <abdinos64@gmail.com>
2022-09-19 16:34:28 +0200

    75% de tp

---
+++ Test/OperationTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationTest {
+
+    @Test
+    void addition() {
+        Operation op = new Operation(1,2);
+        assertEquals(3,op.addition());
+    }
+
+    @Test
+    void substraction() {
+        Operation op = new Operation(1,2);
+        assertEquals(-1,op.substraction());
+    }
+
+    @Test
+    void multiplication() {
+        Operation op = new Operation(1,2);
+        assertEquals(2,op.multiplication());
+    }
+
+    @Test
+    void division() {
+        Operation op = new Operation(1,2);
+        assertEquals(0.5,op.division());
+    }
+}
---
+++ src/MyDate.java
@@ -0,0 +1,40 @@
+public class MyDate {
+   private int day,month,year;
+
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        if (day > 31 || day <1)
+            return;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year < 1)
+            return;
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        if (month > 12 || month <1)
+            return;
+        this.month = month;
+    }
+}
